80c649da4fe12cc56e5e3ed92eebe95325e65e11
Kris Moore <kris@pcbsd.org>
2016-01-05 14:11:28 -0500

    Add a tests/ directory, with some basic REST testing utilities

---
+++ tests/rest-api-test.sh
@@ -0,0 +1,23 @@
+#!/usr/local/bin/bash
+# (Yes, yes, this is a bash script)
+# Both resty/jsawk use bash'isms
+
+# Set variable to call jsawk utility
+JSAWK="./utils/jsawk -j js24"
+
+echo "Enter your username:"
+echo -e ">\c"
+read fuser
+
+echo ""
+echo "Enter your password:"
+echo -e ">\c"
+read -s fpass
+echo ""
+
+# Source our resty functions
+. ./utils/resty -W "http://127.0.0.1:12151" -H "Accept: application/json" -H "Content-Type: application/json" -u ${fuser}:${fpass}
+
+# Check the reply of this REST query
+GET /sysadm/lifepreserver/ '{ "action":"list-cron" }' -v
+
