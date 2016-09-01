f5d9caefed343e9d6cf80ff68eda4d9b9ee749a3
Mrt134 <timmoore88@gmail.com>
2016-09-01 13:37:37 -0400

    Update API reference, client, and server handbooks - Edited link colors to be a stronger blue/red color. - Altered hover role for links to add underlining to the link texts. - Restyled roles   - Unified padding boxes and colors (exception: :guilabel: uses a different color).     All roles should now have the same height padding, which has been reduced as well.   - Role colors have all been lightened to avoid being distracting or making     contained text hard to read.   - Removed bold from :menuselection:.   - :command: now renders as a monospace font. - Restyled admonitions   - Darkened text inside admonition boxes.   - Rounded border elements around boxes.   - Reworked borders to be smaller and darker in color. - Fixed figure captions to be smaller and fit better with the surrounding text. - Restyled tables   - Table captions now have the same style as figure captions.   - Altered color for headers   - Added hover themeing for tables: mousing over a row or header title will change its color.

--- docs/api_reference/conf.py
+++ docs/api_reference/conf.py
@@ -181,10 +181,10 @@ html_logo = 'sysadm_circle_red.png'
 #html_split_index = False
 
 # If true, links to the reST sources are added to the pages.
-#html_show_sourcelink = True
+html_show_sourcelink = False
 
 # If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
-#html_show_sphinx = True
+html_show_sphinx = False
 
 # If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
 #html_show_copyright = True
--- docs/server_handbook/conf.py
+++ docs/server_handbook/conf.py
@@ -181,13 +181,13 @@ html_logo = 'sysadm_circle_red.png'
 #html_split_index = False
 
 # If true, links to the reST sources are added to the pages.
-#html_show_sourcelink = True
+html_show_sourcelink = False
 
 # If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
 html_show_sphinx = False
 
 # If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
-html_show_copyright = False
+html_show_copyright = True
 
 # If true, an OpenSearch description file will be output, and all pages will
 # contain a <link> tag referring to it.  The value of this option must be the
