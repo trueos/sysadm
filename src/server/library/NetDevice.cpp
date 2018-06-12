aab6f484ef01c893ae1701f64732254855eb3cdd
Ken Moore <ken@ixsystems.com>
2018-06-12 09:26:41 -0400

    Remove the IFM_[FDDI/TOKEN] usage in SysAdm. Removed from upstream FreeBSD.

--- src/server/library/NetDevice.cpp
+++ src/server/library/NetDevice.cpp
@@ -174,11 +174,6 @@ QString NetDevice::mediaStatusAsString(){
    QString status;
 
    switch (IFM_TYPE(ifm.ifm_active)){
-      case IFM_FDDI:
-      case IFM_TOKEN:
-         if (ifm.ifm_status & IFM_ACTIVE) status = "inserted";
-         else status = "no ring";
-         break;
       case IFM_IEEE80211:
          if (ifm.ifm_status & IFM_ACTIVE) status = "associated";
          else status = "no carrier";
